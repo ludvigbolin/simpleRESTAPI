c99a3b5931d97552590125f49691e0292c50bc55
ludvigbolin <57351161+ludvigbolin@users.noreply.github.com>
2023-01-23 20:13:52 +0100

    Add DELETE request and GET request
    This adds the function delete_personnel and list_all_personnel.
    All functions are now added without tests

--- api.py
+++ api.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 Items in the database are stored with the email adress as the key and 
 the email adress, first name and last name in a json object as a value
 '''
-database = {}
+database = {
+    -1: {"id": -1, "email": "example@mail.com", "first_name": "example", "last_name": "examplesson"}
+}
 
 id_counter = 0
 email_list = {}
@@ -16,21 +18,59 @@ email_list = {}
 def programming_languages_route():
     if request.method == 'GET':
         return list_all_personnel()
-    elif request.method == 'DELETE':
-        return delete_personnel()
     elif request.method == "POST":
-        return create_new_personnel(request.get_json(force=True))
+        try:
+            json_object = request.get_json(force=True)
+        except:
+            return "Invalid format of json object.", status.HTTP_400_BAD_REQUEST
+        return create_new_personnel(json_object)
     else:
-        return "test"
+        return "No endpoint available with this path.", status.HTTP_400_BAD_REQUEST
+
+
+'''
+delete_personnel takes an id as parameter and deleted that entry from the database. The deleted entry is returned.
+
+Status codes:
+202, successful deletion and returns the deleted entry from the database.
+400, invalid formatting of parameter, should be a integer but is not.
+409, the id requested is not available in the database.
+
+Can be called using:
+[DELETE] /personnel/<input_id>
+<input_id> must be a integer and an id available in the database.
+
+'''
+
+
+@app.delete('/personnel/<input_id>')
+def delete_personnel(input_id):
+    try:
+        input_id_integer = int(input_id)
+    except:
+        return "Parameter is not a integer.", status.HTTP_400_BAD_REQUEST
+
+    if database.get(input_id_integer) != None:
+        entry = database.pop(input_id_integer)
+        email_list.pop(entry["email"])
+
+        return entry, status.HTTP_202_ACCEPTED
+    return "Invalid entry, the id does not exist.", status.HTTP_404_NOT_FOUND
+
+
+'''
+list_all_personnel returns all entries in the database
+
+Status codes:
+200, successful and returns all entries in the database
+
+Can be called using:
+[GET] /personnel 
+'''
 
 
 def list_all_personnel():
-    return {"personnel": list(database.values())}
-
-
-def delete_personnel():
-
-    return
+    return {"personnel": list(database.values())}, status.HTTP_200_OK
 
 
 '''
@@ -63,7 +103,7 @@ def create_new_personnel(json_object_data):
         first_name = json_object_data['first_name']
         last_name = json_object_data['last_name']
     except:
-        return "Invalid input data, wrong format", status.HTTP_400_BAD_REQUEST
+        return "Invalid input data, wrong format.", status.HTTP_400_BAD_REQUEST
 
     if email_list.get(mail) == None:
         database[id_counter] = {"id": id_counter, "email": mail,
@@ -72,4 +112,10 @@ def create_new_personnel(json_object_data):
         id_counter += 1
 
         return str(database[id_counter-1]), status.HTTP_201_CREATED
-    return "Email adress already exists", status.HTTP_409_CONFLICT
+    return "Email adress already exists.", status.HTTP_409_CONFLICT
+
+
+'''
+export FLASK_APP=api.py
+python3 -m flask run
+'''
